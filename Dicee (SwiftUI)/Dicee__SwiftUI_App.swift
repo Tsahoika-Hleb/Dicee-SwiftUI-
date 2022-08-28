ff8e554e283350a676995c84fda526306e08ce8a
Tsahoika Hleb <hlebtsahoika@icloud.com>
2022-08-29 00:41:19 +0300

    Initial Commit

---
+++ Dicee (SwiftUI)/Dicee__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee__SwiftUI_App.swift
+//  Dicee (SwiftUI)
+//
+//  Created by Hleb Tsahoika on 29.08.22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
