50550d3768265d889660f4dcd9440e7a73b8fdf7
Tsahoika Hleb <hlebtsahoika@icloud.com>
2022-09-04 20:12:21 +0300

    Add assets, create Content View and Dice View

--- Dicee (SwiftUI)/ContentView.swift
+++ Dicee (SwiftUI)/ContentView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(diceNumver: leftDiceNumber)
+                    DiceView(diceNumver: rightDiceNumber)
+                }
+                .padding()
+                Spacer()
+                Button("Roll") {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }
+                .font(.system(size: 50))
+                .foregroundColor(.white)
+                .padding(.horizontal)
+                .background(.red)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct DiceView: View {
+    let diceNumver: Int
+    var body: some View {
+        Image("dice\(diceNumver)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
             .padding()
     }
 }
@@ -17,5 +53,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13")
     }
 }
+
+
